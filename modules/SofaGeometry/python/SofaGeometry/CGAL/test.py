83553d11404f6f081103d0fc9d4f0d1e83d528a3
Thomas <thomas.morzadec@inria.fr>
2018-08-29 13:15:24 +0200

    For Damien, before a week off

--- modules/SofaGeometry/python/SofaGeometry/CGAL/test.py
+++ modules/SofaGeometry/python/SofaGeometry/CGAL/test.py
@@ -1,11 +1,17 @@
-import point2D
+from SofaGeometry.CGAl import point2D
+from SofaGeometry.CGAl import polygon2D
+import numpy as np
+point1 = point2D.Point2(0.0, 0.0)
+point2 = point2D.Point2(1.0, 0.0)
+point3 = point2D.Point2(1.0, 1.0)
+point4 = point2D.Point2(0.0, 1.0)
 
-point1 = point2D.Point2(1.0, 5.0)
-point2 = point2D.Point2(1.0, 3.0)
-point1_bis = point2D.Point2(point1)
-print(str(point1.x()) + "   " + str(point2.x()))
-print(str(point1.y()) + "   " + str(point2.y()))
+point2D.test()
 
-print("est equality  " + str(point2D.smaller(point1, point2)))
+#pgn = polygon2D.Polygon2(np.asarray([point1, point2, point3, point4]))
+
+#print(str(pgn.area()))
+
+#print("est equality  " + str(point1 < point2))
 
 
